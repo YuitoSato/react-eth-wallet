68b6ffd7e7246fdc4208257d383dc5c213ae3308
Yuito <yuitosato.w@gmail.com>
2018-07-28 18:13:42 +0900

    一旦保存。promiseじゃないかもだけどできた

---
+++ src/actions/accountAction.js
@@ -0,0 +1,10 @@
+const FETCH_ACCOUNTS = 'FETCH_ACCOUNTS';
+const FETCH_ACCOUNTS_SUCCESS = 'FETCH_ACCOUNTS_SUCCESS';
+const FETCH_ACCOUNTS_ERROR = 'FETCH_ACCOUNTS_ERROR';
+
+export function fetchAccounts(web3) {
+  // return web3.eth.getAccounts()
+
+
+}
+
--- src/components/AccountBalanceBoard.js
+++ src/components/AccountBalanceBoard.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/es/styles/withStyles';
+import Web3 from 'web3';
 
 const styles = theme => ({
   root: {
@@ -20,9 +21,10 @@ const styles = theme => ({
 });
 
 let id = 0;
+
 function createData(name, calories, fat, carbs, protein) {
   id += 1;
-  return { id, name, calories, fat, carbs, protein };
+  return {id, name, calories, fat, carbs, protein};
 }
 
 const data = [
@@ -36,43 +38,115 @@ const data = [
 class AccountBalanceBoard extends Component {
   constructor(props) {
     super(props);
+    this.web3 = new Web3();
+    this.web3.setProvider(new this.web3.providers.HttpProvider("http://localhost:8545"));
+
+    // NOTE yuito
+    // https://github.com/coopermaruyama/react-web3/issues/25
+    // windowのweb3を塗り替える。
+    if (window.web3) {
+      window.web3 = this.web3;
+    }
+
+    this.state = {
+      accounts: ['aaa'],
+      blockNumber: 0,
+      loading: true
+    };
+  }
+
+  componentWillMount() {
+    this.load();
+  }
+
+  load() {
+    // const blockNumber = this.web3.eth.getBlockNumber().catch(e => e);
+    //
+    // if (!blockNumber) {
+    //   console.log("やばい");
+    //   return;
+    // }
+
+    this.web3.eth.getBlockNumber((e, r) => {
+      if (e) {
+        console.log(e);
+      }
+      console.log(r);
+      const newState = Object.assign({}, this.state, {
+        accounts: ['bbb'],
+        blockNumber: r,
+        loading: false
+      });
+      this.setState(newState);
+    });
+
+    // const accounts = await this.web3.eth.getAccounts(function(e, r) {
+    //   if(!e) {
+    //     console.log(r);
+    //     return r;
+    //   } else {
+    //     console.log('やばいい');
+    //   }
+    // });
+    // console.log(accounts);
+    // if (accounts instanceof Error || accounts == null) {
+    //   console.log('やばい');
+    //   return;
+    // }
+
+    // const accounts = this.web3.eth.getAccounts(function (e, r) {
+    //   if (!e) {
+    //     console.log(r);
+    //     return r;
+    //   } else {
+    //     console.log('やばいい');
+    //   }
+    // });
+    // console.log(accounts)
+    //
   }
 
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
 
-    return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell numeric>Calories</TableCell>
-              <TableCell numeric>Fat (g)</TableCell>
-              <TableCell numeric>Carbs (g)</TableCell>
-              <TableCell numeric>Protein (g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map(n => {
-              return (
-                <TableRow key={n.id}>
+    if (this.state.loading) {
+      return (<div>
+        ロード中
+      </div>);
+    } else {
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell numeric>Calories</TableCell>
+                <TableCell numeric>Fat (g)</TableCell>
+                <TableCell numeric>Carbs (g)</TableCell>
+                <TableCell numeric>Protein (g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.accounts.map((account, i) => (
+                <TableRow key={i}>
                   <TableCell component="th" scope="row">
-                    {n.name}
+                    {account}
                   </TableCell>
-                  <TableCell numeric>{n.calories}</TableCell>
-                  <TableCell numeric>{n.fat}</TableCell>
-                  <TableCell numeric>{n.carbs}</TableCell>
-                  <TableCell numeric>{n.protein}</TableCell>
+                  <TableCell numeric>{this.state.blockNumber}</TableCell>
+                  <TableCell numeric>あああ</TableCell>
+                  <TableCell numeric>いいい</TableCell>
+                  <TableCell numeric>ううう</TableCell>
                 </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Paper>
-    )
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      )
+    }
+
   }
 }
+
 
 // AccountBalanceBoard.propTypes = {
 //   classes: PropTypes.object.isRequired,
