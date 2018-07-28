c04b5d76490a8af16220345e8c32655b9971ee00
Yuito <yuitosato.w@gmail.com>
2018-07-28 14:51:23 +0900

    : )

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import AccountBalanceBoard from './components/AccountBalanceBoard';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AccountBalanceBoard>
+
+      </AccountBalanceBoard>
+    );
+  }
+}
+
+export default App;
